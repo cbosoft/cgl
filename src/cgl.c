ec2109ae1f06c1ab7c10a9269e60a5319c6dd516
Chris Boyle <chrismjboyle@gmail.com>
2020-02-04 15:04:09 +0000

    Added newline after hello world

--- src/cgl.c
+++ src/cgl.c
@@ -2,7 +2,7 @@
 
 void cgl_helloWorld()
 {
-  fprintf(stderr, "Hello, world!");
+  fprintf(stderr, "Hello, world!\n");
 }
 
 // https://en.wikibooks.org/wiki/OpenGL_Programming/Scientific_OpenGL_Tutorial_01
