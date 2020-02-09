e0fee2bbf7b29d5721cd175af255d8799cc3d94d
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-02-09 10:21:34 +0000

    Started to flesh out API: line/axes/figure abstraction: drawing

--- src/cgl.c
+++ src/cgl.c
@@ -5,6 +5,59 @@
 #include "cgl.h"
 
 
+cgl_Figure *cgl_init_figure()
+{
+  cgl_Figure *fig = calloc(1, sizeof(cgl_Figure));
+  fig->axes = cgl_create_axes();
+  return fig;
+}
+
+cgl_Axes *cgl_create_axes()
+{
+  cgl_Axes *ax = calloc(1, sizeof(cgl_Axes));
+  ax->x_label = "x";
+  ax->y_label = "y";
+  ax->x_lim[0] = 0.0;
+  ax->x_lim[1] = 1.0;
+  ax->y_lim[0] = 0.0;
+  ax->y_lim[1] = 1.0;
+  return ax;
+}
+
+cgl_float cgl_axes_get_span_x(cgl_Axes *ax)
+{
+  cgl_float max = ax->x_lim[1];
+  cgl_float min = ax->x_lim[0];
+  return max - min;
+}
+
+cgl_float cgl_axes_get_span_y(cgl_Axes *ax)
+{
+  cgl_float max = ax->y_lim[1];
+  cgl_float min = ax->y_lim[0];
+  return max - min;
+}
+
+
+cgl_LineStyle *cgl_create_style()
+{
+  cgl_LineStyle *style = calloc(1, sizeof(cgl_LineStyle));
+  style->r = 0.0;
+  style->g = 0.0;
+  style->b = 1.0;
+  style->a = 1.0;
+  return style;
+}
+
+cgl_Line *cgl_create_line()
+{
+  cgl_Line *line = calloc(1, sizeof(cgl_Line));
+  line->style = cgl_create_style();
+  return line;
+}
+
+
+
 void line_to(cairo_t *cr, guint x1, guint y1, guint x2, guint y2)
 {
   cairo_move_to(cr, x1, y1);
@@ -12,12 +65,33 @@ void line_to(cairo_t *cr, guint x1, guint y1, guint x2, guint y2)
 }
 
 
-gboolean draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
+void cgl_plot (cgl_Figure *figure, cgl_float *x, cgl_float *y, cgl_uint npoints)
+{
+  (void) figure;
+  
+  cgl_Line *line = cgl_create_line();
+  line->points = calloc(npoints, sizeof(cgl_Point*));
+  // TODO error checking on alloc
+  line->npoints = npoints;
+  
+  for (cgl_uint point_i = 0; point_i < npoints; point_i++) {
+    line->points[point_i] = calloc(1, sizeof(cgl_Point));
+    line->points[point_i]->x = x[point_i];
+    line->points[point_i]->y = y[point_i];
+    fprintf(stderr, "Added point (%f,%f) to line\n", x[point_i], y[point_i]);
+  }
+
+  figure->lines = realloc(figure->lines, (++figure->nlines)*sizeof(cgl_Line));
+  figure->lines[figure->nlines-1] = line;
+  fprintf(stderr, "Added line\n");
+}
+
+gboolean cgl_painter_cb (GtkWidget *widget, cairo_t *cr, gpointer data)
 {
   (void) widget;
-  cgl_Line *line_data = (cgl_Line *)data;
+  cgl_Figure *fig = (cgl_Figure *)data;
 
-  if (line_data == NULL)
+  if (fig == NULL)
     return FALSE;
 
   guint width = gtk_widget_get_allocated_width(widget);
@@ -27,60 +101,58 @@ gboolean draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
   cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
   cairo_paint(cr);
 
+  cgl_uint margin_left = (guint)(0.1*width);
+  cgl_uint margin_right = (guint)(0.1*width);
+  cgl_uint margin_top = (guint)(0.1*height);
+  cgl_uint margin_bottom = (guint)(0.1*height);
+
   // Axes
   cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
-  line_to(cr, (guint)(0.1*width), (guint)(0.9*height), (guint)(0.9*width), (guint)(0.9*height));
-  line_to(cr, (guint)(0.1*width), (guint)(0.9*height), (guint)(0.1*width), (guint)(0.1*height));
+  line_to(cr, margin_left, height-margin_bottom, width-margin_right, height-margin_top);
+  line_to(cr, margin_left, height-margin_bottom, margin_left, margin_top);
+  cairo_stroke(cr);
+
+  // Labels
+  cairo_select_font_face(cr, "Courier", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+  cairo_set_font_size(cr, 12);
+
+  cairo_text_extents_t extents;
+  cairo_text_extents(cr, "X-LABEL", &extents);
+  cairo_move_to(cr, width/2 - extents.width/2, height-(margin_bottom/2));
+  cairo_show_text(cr, "X-LABEL");
+  //cairo_save(cr); 
+  //cairo_rotate(cr, 90);
+  cairo_text_extents(cr, "Y-LABEL", &extents);
+  cairo_move_to(cr, margin_left/2, height/2-extents.height/2);
+  //cairo_move_to(cr, width/2 - extents.width/2, height-(margin_bottom/2));
+  cairo_show_text(cr, "Y-LABEL");
+  //cairo_restore(cr);
+
+  // Data
+  cgl_float px_per_data_x = ((cgl_float)(width-margin_left-margin_right)) / cgl_axes_get_span_x(fig->axes);
+  cgl_float px_per_data_y = ((cgl_float)(width-margin_top-margin_bottom)) / cgl_axes_get_span_y(fig->axes);
+#define CGL_SCALE_X(F) (cgl_uint)(px_per_data_x * F)
+#define CGL_SCALE_Y(F) (cgl_uint)(px_per_data_y * F)
+  for (cgl_uint line_i = 0; line_i < fig->nlines; line_i++) {
+    fprintf(stderr, "Plotting line\n");
+    cgl_Line *line = fig->lines[line_i];
+    if (!line->npoints)
+      continue;
+
+    cgl_Point *point = line->points[0];
+    cairo_move_to(cr, margin_left + CGL_SCALE_X(point->x), height-(margin_bottom+CGL_SCALE_Y(point->y)));
+    // TODO draw point if style requires it
+
+    cairo_set_source_rgba(cr, line->style->r, line->style->g, line->style->b, line->style->a);
+
+    for (cgl_uint point_i = 1; point_i < line->npoints; point_i++) {
+      point = line->points[point_i];
+      cairo_line_to(cr, margin_left + CGL_SCALE_X(point->x), height-(margin_bottom+CGL_SCALE_Y(point->y)));
+    }
+  }
+#undef CGL_SCALE_Y
+#undef CGL_SCALE_X
   cairo_stroke(cr);
 
   return FALSE;
-}
-
-
-
-static void activate (GtkApplication* app, gpointer data)
-{
-
-  GtkWidget *window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 700, 700);
-
-  GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-  gtk_container_add(GTK_CONTAINER(window), box);
-
-  GtkWidget *title_label = gtk_label_new("TITLE");
-  gtk_container_add(GTK_CONTAINER(box), title_label);
-
-  GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-  gtk_container_add(GTK_CONTAINER(box), hbox);
-
-  GtkWidget *y_label = gtk_label_new("Y LABEL");
-  gtk_label_set_angle(GTK_LABEL(y_label), 90);
-  gtk_container_add(GTK_CONTAINER(hbox), y_label);
-
-  GtkWidget *plotter = gtk_drawing_area_new();
-  gtk_widget_set_size_request(plotter, 600, 300);
-  gtk_widget_set_hexpand(plotter, TRUE);
-  gtk_widget_set_vexpand(plotter, TRUE);
-  g_signal_connect(G_OBJECT(plotter), "draw",
-                    G_CALLBACK(draw_callback), data);
-  gtk_container_add(GTK_CONTAINER(hbox), plotter);
-
-  GtkWidget *x_label = gtk_label_new("X LABEL");
-  gtk_container_add(GTK_CONTAINER(box), x_label);
-
-  gtk_widget_show_all (window);
-}
-
-void cgl_plot (cgl_Line *line)
-{
-  int argc = 1;
-  char *argv[1] = {"test"};
-  GtkApplication *app;
-
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), line);
-  g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
-
 }
--- src/cgl.h
+++ src/cgl.h
@@ -1,13 +1,57 @@
 #pragma once
+#include <gtk/gtk.h>
 
 typedef double cgl_float;
 typedef unsigned int cgl_uint;
 
 typedef struct {
-  cgl_float *x;
-  cgl_float *y;
-  cgl_uint len;
-  char *style;
+  cgl_float x;
+  cgl_float y;
+} cgl_Point;
+
+typedef struct {
+  cgl_float r;
+  cgl_float g;
+  cgl_float b;
+  cgl_float a;
+} cgl_LineStyle;
+
+typedef struct {
+  cgl_Point **points;
+  cgl_uint npoints;
+  cgl_LineStyle *style;
 } cgl_Line;
 
-void cgl_plot();
+typedef struct {
+  char *x_label;
+  char *y_label;
+  cgl_float x_lim[2];
+  cgl_float y_lim[2];
+} cgl_Axes;
+
+typedef struct {
+  cgl_Axes *axes;
+  cgl_Line **lines;
+  cgl_uint nlines;
+  /*char *title*/
+} cgl_Figure;
+
+cgl_Figure *cgl_init_figure();
+cgl_Axes *cgl_create_axes();
+cgl_Line *cgl_create_line();
+cgl_LineStyle *cgl_create_style();
+
+char *cgl_axes_get_ylabel(cgl_Axes *ax);
+char *cgl_axes_get_xlabel(cgl_Axes *ax);
+void cgl_axes_set_xlabel(cgl_Axes *ax, char *lbl);
+void cgl_axes_set_xlabel(cgl_Axes *ax, char *lbl);
+
+void cgl_axes_get_xlim(cgl_Axes *ax, cgl_float *left, cgl_float *right);
+void cgl_axes_get_ylim(cgl_Axes *ax, cgl_float *bottom, cgl_float *top);
+void cgl_axes_set_xlim(cgl_Axes *ax, cgl_float left, cgl_float right);
+void cgl_axes_set_ylim(cgl_Axes *ax, cgl_float bottom, cgl_float top);
+
+void cgl_plot (cgl_Figure *figure, cgl_float *x, cgl_float *y, cgl_uint npoints);
+gboolean cgl_painter_cb (GtkWidget *widget, cairo_t *cr, gpointer data);
+
+// vim: ft=c
